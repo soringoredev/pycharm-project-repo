e83b252ec38b21e39d1695613a5a814b5a82f7e8
Alexandru Scripchin <sripchin.sasa@gmail.com>
2025-12-23 22:17:40 +0200

    adaug alex.py

---
+++ alex/alex.py
@@ -0,0 +1,11 @@
+varsta = int(input("Introdu varsta ta:"))
+if varsta > 65:
+    print('pensie')
+else:
+    print("Lucreaza mai departe")
+
+if varsta >=18:
+        print("adult")
+else:
+    print("copil")
+
